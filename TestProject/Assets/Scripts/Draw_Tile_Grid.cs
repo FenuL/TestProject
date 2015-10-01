77813b73a2628f14225f94a40117e7c263d8b034
FenuL <luciano.something@gmail.com>
2015-09-30 20:34:30 -0400

    Movement update
    Lots of updates

--- TestProject/Assets/Scripts/Draw_Tile_Grid.cs
+++ TestProject/Assets/Scripts/Draw_Tile_Grid.cs
@@ -1,132 +1,173 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
-public class Tile : MonoBehaviour {
+public class Tile {
 	//variables
-	int tile_height;
-	int tile_sprite;
-	int tile_x_index;
-	int tile_y_index;
-	double tile_x_pos;
-	double tile_y_pos;
+	Transform tileObj;
+	int tileXIndex;
+	int tileYIndex;
+	int tileHeight;
+	int tileSpriteIndex;
 
 	//methods
-	public Tile(int x_index, int y_index, double x_pos, double y_pos, int height, int sprite){
-		tile_x_index = x_index;
-		tile_y_index = y_index;
-		tile_x_pos = x_pos;
-		tile_y_pos = y_pos;
-		tile_height = height;
-		tile_sprite = sprite;
+	public Tile(Transform tile, int x_index, int y_index, int height, int sprite){
+		tileObj = tile;
+		tileXIndex = x_index;
+		tileYIndex = y_index;
+		tileHeight = height;
+		tileSpriteIndex = sprite;
 	}
 
-	public Tile(int x_index, int y_index, double x_pos, double y_pos, int height){
-		tile_x_index = x_index;
-		tile_y_index = y_index;
-		tile_x_pos = x_pos;
-		tile_y_pos = y_pos;
-		tile_height = height;
-		tile_sprite = 1;
+	public Tile(Transform tile, int x_index, int y_index, int height){
+		tileObj = tile;
+		tileXIndex = x_index;
+		tileYIndex = y_index;
+		tileHeight = height;
+		tileSpriteIndex = 1;
 	}
 
-	public int get_x_index(){
-		return tile_x_index;
+	public Tile(int x_index, int y_index, int height){
+		tileXIndex = x_index;
+		tileYIndex = y_index;
+		tileHeight = height;
+		tileSpriteIndex = 1;
+	}
+
+	public int getXIndex(){
+		return tileXIndex;
 	}
 	
-	public int get_y_index(){
-		return tile_y_index;
+	public int getYIndex(){
+		return tileYIndex;
 	}
 
-	public double get_x_pos(){
-		return tile_x_pos;
+	public Transform getObj (){
+		return tileObj;
 	}
 
-	public double get_y_pos(){
-		return tile_y_pos;
+	public void setObj (Transform tile){
+		tileObj = tile;
 	}
 
-	public int get_height(){
-		return tile_height;
+	public int getHeight(){
+		return tileHeight;
 	}
 
-	public int get_tile_sprite(){
-		return tile_sprite;
+	public int getTileSpriteIndex(){
+		return tileSpriteIndex;
 	}
 
-	public void set_tile_sprite(int i){
-		tile_sprite = i;
+	public void setTileSpriteIndex(int i){
+		tileSpriteIndex = i;
 	}
 }
 
-public class Tile_Grid : MonoBehaviour {
-	double TILE_LENGTH = 43;
-	double TILE_WIDTH = 85;
-	double TILE_HEIGHT = 12;
-	static int tile_grid_width=20;
-	static int tile_grid_height=20;
-	Tile[,] tiles = new Tile[tile_grid_width, tile_grid_height];
+public class Tile_Grid : ScriptableObject{
+	public float TILE_LENGTH = 43;
+	public float TILE_WIDTH = 85;
+	public float TILE_HEIGHT = 12;
+	public static int tile_grid_width=20;
+	public static int tile_grid_height=20;
+	Transform tile;
+	Sprite[] tileSpriteSheet;
+	SpriteRenderer sprite;
+	Tile[,,] tiles = new Tile[tile_grid_width, tile_grid_height,10];
+	int[,] tile_heights = new int[tile_grid_width, tile_grid_height];
+	int[,] tile_sprites = new int[tile_grid_width, tile_grid_height];
 
-	public Tile_Grid(string[] lines){
+	public Tile_Grid(string[] lines, Transform tile_prefab, Sprite[] newTileSprites){
+		tile = tile_prefab;
+		tileSpriteSheet = newTileSprites;
 		//read the height map
 		int tile_height;
 		int tile_sprite;
-		int line_num = 0;
-		int num_num = 0;
+		int tile_number = 0;
+		int row_num = 0;
+		int col_num = 0;
 		double start_x_pos = 0;
 		double start_y_pos = 4;
 		foreach (string line in lines){
 			string[] elements = line.Split(' ');
 			foreach (string s in elements){
-				if (line_num == 1) { 
-					if (num_num==0){
+				if (row_num == 1) { 
+					if (col_num==0){
 						if (int.TryParse(s, out tile_grid_width)){
 						}
 					}
-					else if (num_num == 1) {
+					else if (col_num == 1) {
 						if (int.TryParse(s, out tile_grid_height)){
 						}
 					}
 				}
-				else if (line_num >= 3 && line_num <= tile_grid_height+3){
+				else if (row_num >= 3 && row_num <= tile_grid_height+3){
 					if (int.TryParse(s, out tile_height)){
-						double tile_x_pos = start_x_pos - (line_num-3) * (TILE_WIDTH/200) + (num_num) * (TILE_WIDTH/200);
-						double tile_y_pos = start_y_pos - (line_num-3) * (TILE_LENGTH/200) - (num_num) * (TILE_LENGTH/200);
+						tile_heights[row_num-3,col_num] = tile_height;
+						//double tile_x_pos = start_x_pos - (row_num-3) * (TILE_WIDTH/200) + (col_num) * (TILE_WIDTH/200);
+						//double tile_y_pos = start_y_pos - (row_num-3) * (TILE_LENGTH/200) - (col_num) * (TILE_LENGTH/200);
 						//print("line_num:" + line_num);
 						//print ("num_num:" + num_num);
 						//print ("x_pos:" + tile_x_pos);
 						//print ("y_pos:" + tile_y_pos);
-						tiles[line_num-3,num_num] = new Tile(line_num-3, num_num, tile_x_pos, tile_y_pos, tile_height);
+						//tiles[row_num-3,col_num] = new Tile(row_num-3, col_num, tile_x_pos, tile_y_pos, tile_height);
 					}
 				}
-				else if(line_num >= tile_grid_height+4){
+				else if(row_num >= tile_grid_height+4){
 					if (int.TryParse(s, out tile_sprite)){
+						tile_sprites[row_num-tile_grid_height-4,col_num] = tile_sprite;
 						//print("line_num:" + line_num);
 						//print ("line_num - tile_grid_height - 4 = " + (line_num-tile_grid_height-4));
 						//print ("num_num:" + num_num);
-						tiles[line_num-tile_grid_height-4,num_num].set_tile_sprite(tile_sprite);
+						//tiles[row_num-tile_grid_height-4,col_num].setTileSpriteIndex(tile_sprite);
 					}
 				}
-				num_num++;
+				col_num++;
 			}
-			num_num = 0;
-			line_num++;
-
+			col_num = 0;
+			row_num++;
 		}
-
-		//create the tiles
-		foreach (Tile t in tiles){
-
-			//var tile_object = new GameObject("tile");
-			//tile_object.AddComponent();
-			//GameObject tile_object = new GameObject("tile");
-			//cube.AddComponent<Rigidbody>();
-			//cube.transform.position = new Vector3(x, y, 0);
-			//GameObject tile = (GameObject) Instantiate (tile_object, new Vector3(t.get_x_pos(), t.get_y_pos(), 0));
+		GameObject Tile_Grid;
+		TileData script;
+		for (int x = 0; x < tile_grid_width; x++){
+			for (int y = 0; y < tile_grid_height; y++){
+				for (int z=0; z < tile_heights[x,y]; z++){
+					//Set the correct sprite for the tile
+					sprite = tile.GetComponent<SpriteRenderer>();
+					sprite.sprite = tileSpriteSheet[tile_sprites[x,y]-1];
+					
+					//Instantiate the tile object. Destroy the collider on the prefab if the tile is not on top of the stack. 
+					if (z != tile_heights[x,y]-1){
+						Destroy (((Transform)Instantiate(tile, new Vector3((float)(start_x_pos - (x) * (TILE_WIDTH/200) + (y) * (TILE_WIDTH/200)), (float)(start_y_pos - (x) * (TILE_LENGTH/200) - (y) * (TILE_LENGTH/200)+z*TILE_HEIGHT/100.0), 0), Quaternion.identity)).gameObject.GetComponent<PolygonCollider2D>());
+					}
+					else {
+						Instantiate(tile, new Vector3((float)(start_x_pos - (x) * (TILE_WIDTH/200) + (y) * (TILE_WIDTH/200)), (float)(start_y_pos - (x) * (TILE_LENGTH/200) - (y) * (TILE_LENGTH/200)+z*TILE_HEIGHT/100.0), 0), Quaternion.identity);
+					}
+					//Increase the tile draw order so other tiles are drawn correctly.
+					if (sprite)
+					{
+						tile_number++;
+						sprite.sortingOrder = tile_number;
+					}
+					tiles[x,y,z] = new Tile(tile,x,y,tile_heights[x,y],tile_sprites[x,y]);
+					script = tile.GetComponent<TileData>();
+					script.x_index = x;
+					script.y_index = y;
+					script.z_index = z;
+					script.tile_height = tile_heights[x,y];
+					script.tile_sprite_index = tile_sprites[x,y];
+				}
+			}
 		}
+		sprite.sortingOrder = 0;
+
 	}
 
-	public Tile[,] get_tiles(){
+	public Tile[,,] getTiles(){
 		return tiles;
+	}
+
+	public Tile getTopTile(int x, int y){
+		return tiles [x, y, tiles [x, y, 0].getHeight () - 1];
 	}
 
 	public double get_TILE_HEIGHT(){
@@ -136,108 +177,14 @@ public class Tile_Grid : MonoBehaviour {
 
 public class Draw_Tile_Grid : MonoBehaviour {
 
-	public Transform Tile1;
-	public Transform Tile2;
-	public Transform Tile3;
-	public Transform Tile4;
-	private Tile_Grid tile_grid;
+	public Transform tile;
+	public Sprite[] tileSpriteSheet;
+	public Tile_Grid tileGrid;
 
 	// Use this for initialization
 	void Start () {
-		int tile_number = 0;
 		string[] lines = System.IO.File.ReadAllLines(@"Assets/height_map.txt");
-		tile_grid = new Tile_Grid(lines);
-		SpriteRenderer sprite1 = Tile1.GetComponent<SpriteRenderer>();
-		SpriteRenderer sprite2 = Tile2.GetComponent<SpriteRenderer>();
-		SpriteRenderer sprite3 = Tile3.GetComponent<SpriteRenderer>();
-		SpriteRenderer sprite4 = Tile4.GetComponent<SpriteRenderer>();
-		Transform tran;
-		GameObject obj;
-		foreach (Tile t in tile_grid.get_tiles()) {
-
-			for (int i=0; i < t.get_height(); i++){
-
-				if (t.get_tile_sprite() == 1){
-
-					if (i != t.get_height()-1){
-						Destroy (((Transform)Instantiate(Tile1, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity)).gameObject.GetComponent<PolygonCollider2D>());
-						sprite1 = Tile1.GetComponent<SpriteRenderer>();
-					}
-					else {
-						Instantiate(Tile1, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity);
-						sprite1 = Tile1.GetComponent<SpriteRenderer>();
-					}
-				}
-				else if (t.get_tile_sprite() == 2){
-					if (i != t.get_height()-1){
-						Destroy (((Transform)Instantiate(Tile2, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity)).gameObject.GetComponent<PolygonCollider2D>());
-						sprite1 = Tile2.GetComponent<SpriteRenderer>();
-					}
-					else {
-						Instantiate(Tile2, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity);
-						sprite1 = Tile2.GetComponent<SpriteRenderer>();
-					}
-				}
-				else if (t.get_tile_sprite() == 3){
-					if (i != t.get_height()-1){
-						Destroy (((Transform)Instantiate(Tile3, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity)).gameObject.GetComponent<PolygonCollider2D>());
-						sprite1 = Tile3.GetComponent<SpriteRenderer>();
-					}
-					else {
-						Instantiate(Tile3, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity);
-						sprite1 = Tile3.GetComponent<SpriteRenderer>();
-					}
-				}
-				else if (t.get_tile_sprite() == 4){
-					if (i != t.get_height()-1){
-						Destroy (((Transform)Instantiate(Tile4, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity)).gameObject.GetComponent<PolygonCollider2D>());
-						sprite1 = Tile4.GetComponent<SpriteRenderer>();
-					}
-					else {
-						Instantiate(Tile4, new Vector3((float)t.get_x_pos(), (float)(t.get_y_pos()+i*tile_grid.get_TILE_HEIGHT()/100.0), 0), Quaternion.identity);
-						sprite1 = Tile4.GetComponent<SpriteRenderer>();
-					}
-				}
-				if (sprite1)
-				{
-					tile_number++;
-					sprite1.sortingOrder = tile_number;
-
-					//sprite.sortingOrder = tile_number;
-					//Renderer.sortingOrder
-				}
-				if (sprite2)
-				{
-					tile_number++;
-					sprite2.sortingOrder = tile_number;
-					
-					//sprite.sortingOrder = tile_number;
-					//Renderer.sortingOrder
-				}
-				if (sprite3)
-				{
-					tile_number++;
-					sprite3.sortingOrder = tile_number;
-					
-					//sprite.sortingOrder = tile_number;
-					//Renderer.sortingOrder
-				}
-				if (sprite4)
-				{
-					tile_number++;
-					sprite4.sortingOrder = tile_number;
-					
-					//sprite.sortingOrder = tile_number;
-					//Renderer.sortingOrder
-				}
-
-			}
-
-		}
-		sprite1.sortingOrder = 0;
-		sprite2.sortingOrder = 0;
-		sprite3.sortingOrder = 0;
-		sprite4.sortingOrder = 0;
+		tileGrid = new Tile_Grid(lines, tile, tileSpriteSheet);
 	}
 	
 	// Update is called once per frame
---
+++ TestProject/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour{
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public GameObject Tile_Grid;
+	public Draw_Tile_Grid script;
+	private TileData data;
+	private Tile[,,] tiles;
+	private Transform currentTile;
+	private Transform selectedTile;
+	private bool initialized = false;
+	//public SpriteRenderer renderer;
+
+	// Update is called once per frame
+	void Update () {
+		if (! initialized) {
+			Initialize ();
+			initialized = true;
+		}
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit2D hit;
+
+		/*if (Physics.Raycast (ray, out hit)) {
+			// if the ray hit a collider, we'll get the world-coordinate here.
+			Vector3 worldPos = hit.point;
+			print("x="+ worldPos.x + ", y=" + worldPos.y);
+		}*/
+		hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		
+		//hit: RaycastHit2D = Physics2D.Raycast(transform.position, -Vector2.up);
+		//hit = Physics2D.GetRayIntersection (ray, Mathf.Infinity, 0);
+		if (hit){
+			data = hit.transform.GetComponent<TileData>();
+			print ("x: " + data.x_index + ", y: " + data.y_index + ", z: " + data.z_index);
+			//if (currentTile != selectedTile){
+			currentTile.GetComponent<SpriteRenderer> ().color = new Color(255f, 255f, 255f, 1f);
+			currentTile = hit.transform;
+			transform.position = new Vector3(currentTile.position.x, currentTile.position.y+(float)(script.tileGrid.TILE_LENGTH/200.0), currentTile.position.z);
+				//renderer = (SpriteRenderer)currentTile.GetComponent<SpriteRenderer> ();
+			currentTile.GetComponent<SpriteRenderer> ().color = new Color(0f, 0f, 0f, 1f); // Set to opaque black
+				//print("test");
+			//}
+			//Vector3 worldPos = hit.point;
+			//print("x="+ worldPos.x + ", y=" + worldPos.y);
+		}
+
+		if(Input.GetMouseButton(0)){
+			//print("test2");
+			selectedTile.GetComponent<SpriteRenderer> ().color=new Color(255f, 255f, 255f, 1f); // Set to white
+			selectedTile = currentTile;
+			selectedTile.GetComponent<SpriteRenderer> ().color=new Color(255f, 0f, 0f, 1f); // Set to blue
+
+		}
+
+
+
+	}
+
+	void Initialize(){
+		script = Tile_Grid.GetComponent<Draw_Tile_Grid>();
+		tiles = script.tileGrid.getTiles ();
+		currentTile = tiles[0, 0, 0].getObj ();
+		selectedTile = tiles [0, 0, 0].getObj ();
+	}
+}
